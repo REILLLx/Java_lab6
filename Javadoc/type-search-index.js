794ed28352fb59ab12833878a366d7d24c084e46
REILLLx <tuman.kv@gmail.com>
2024-12-29 00:25:34 +0200

    Javadoc-add

---
+++ Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"railway","l":"BaggageCar"},{"p":"<Unnamed>","l":"LinkedSet"},{"p":"<Unnamed>","l":"Main"},{"p":"railway","l":"PassengerCar"},{"p":"railway","l":"RollingStock"},{"p":"railway","l":"SleepingCar"},{"p":"railway","l":"Train"}];updateSearchResults();
